905af915698e3f0a38e068a07aa8ca57fcda3d24
Piotr Wilczyński <delwing@gmail.com>
2025-06-30 18:08:35 +0200

    tests that actually tests

--- spec/character_oslabienie_spec.lua
+++ spec/character_oslabienie_spec.lua
@@ -2,9 +2,10 @@
 
 describe("skrypty.character.oslabienie", function()
   it("adds progress information to the output", function()
+    setWindowWrap(1000000)
     echo("Running test")
     alias_func_skrypty_fake_combat("Twoje cechy sa oslabione po ostatniej smierci. By je odbudowac potrzebujesz zdobyc jeszcze minimalne postepy.")
-    moveCursor(0, getLastLineNumber())
+    moveCursor(0, getLastLineNumber() - 2)
     local line = copy2decho()
     assert.is_truthy(string.find(line, "minimalne"))
     assert.is_truthy(string.find(line, "%[0/15%]"))
